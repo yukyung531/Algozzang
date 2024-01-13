f0f265fe3703d1a45336d4d33c929d9d59d13137
yukyung531 <yukyung531@gmail.com>
2024-01-13 19:57:58 +0900

    BOJ_11656_접미사 배열

---
+++ 권유경/BOJ/BOJ_11656_접미사 배열.java
@@ -0,0 +1,35 @@
+package BAEK_11656_접미사배열;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String S = sc.next();
+
+//		String[] arr = S.split("");
+
+//		for (int i = 0; i < S.length(); i++) {
+//			newArr[i] = arr[i];
+//			for (int j = i+1; j < S.length(); j++) {
+//					newArr[i] += arr[j];
+//			}
+//			// i
+//		}
+//		// n
+		
+		String[] arr = new String[S.length()];
+		for(int i =0; i<S.length(); i++) {
+			arr[i] = S.substring(i, S.length());
+		}
+		Arrays.sort(arr);
+		for(String str : arr) {
+			System.out.println(str);
+		}
+
+	}
+	// main
+}
